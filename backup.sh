eb28bbbe5a9b11fc594c2b0ec497c3f2755f1b69
Joseph Garnier <garnjose@gmail.com>
2020-04-16 12:09:18 +0200

    add licence

--- backup.sh
+++ backup.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
-# Author Joseph Garnier
+# Copyright 2019-present, Joseph Garnier
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 
+#!/bin/bash
 source utility.sh
 source tasks.sh
 
--- tasks.sh
+++ tasks.sh
@@ -1,5 +1,10 @@
+# Copyright 2019-present, Joseph Garnier
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 #!/bin/bash
-# Author Joseph Garnier
 
 #######################################
 # Backup the PPA source list
