b62945161f51062b19d67e5c61a9bd6a4e2baaff
Joseph Garnier <garnjose@gmail.com>
2020-04-29 22:56:58 +0200

    add a guard to include the file once

--- src/global.sh
+++ src/global.sh
@@ -6,14 +6,19 @@
 
 #!/bin/bash
 
-# see https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-readonly PROJECT_DIR="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")"
-readonly PROJECT_CONFIG_DIR="${PROJECT_DIR}/config"
-readonly PROJECT_LOG_DIR="${PROJECT_DIR}/log"
-readonly PROJECT_SRC_DIR="${PROJECT_DIR}/src"
-readonly PROJECT_TEMP_DIR="${PROJECT_DIR}/temp"
+if [[ ! -v BACKUP_TOOL_GLOBAL ]]; then
+	readonly BACKUP_TOOL_GLOBAL=true
 
-readonly PROJECT_LSYNCD_PID_FILE="${PROJECT_LOG_DIR}/lsyncd.pid"
-readonly PROJECT_LSYNCD_STATUS_FILE="${PROJECT_LOG_DIR}/lsyncd.status"
-readonly PROJECT_LSYNCD_CONFIG_FILE="${PROJECT_CONFIG_DIR}/lsyncd.conf.in"
-readonly PROJECT_LSYNCD_TEMPLATE_CONFIG_FILE="${PROJECT_CONFIG_DIR}/lsyncd.conf"
+	# see https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+	readonly PROJECT_DIR="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")"
+	readonly PROJECT_CONFIG_DIR="${PROJECT_DIR}/config"
+	readonly PROJECT_LOG_DIR="${PROJECT_DIR}/log"
+	readonly PROJECT_SRC_DIR="${PROJECT_DIR}/src"
+	readonly PROJECT_TEMP_DIR="${PROJECT_DIR}/temp"
+	readonly PROJECT_VAR_DIR="${PROJECT_DIR}/var"
+
+	readonly PROJECT_LSYNCD_PID_FILE="${PROJECT_VAR_DIR}/lsyncd.pid"
+	readonly PROJECT_LSYNCD_STATUS_FILE="${PROJECT_LOG_DIR}/lsyncd.status"
+	readonly PROJECT_LSYNCD_CONFIG_FILE="${PROJECT_CONFIG_DIR}/lsyncd.conf.in"
+	readonly PROJECT_LSYNCD_TEMPLATE_CONFIG_FILE="${PROJECT_CONFIG_DIR}/lsyncd.conf"
+fi
