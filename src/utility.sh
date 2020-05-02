ccdfefd6c89531cfc25ed57c4687b4d7f40d1335
Joseph Garnier <garnjose@gmail.com>
2020-05-02 23:23:05 +0200

    rewrite all functions and add some new

--- src/utility.sh
+++ src/utility.sh
@@ -16,26 +16,33 @@ readonly -A COLORS=( \
 #######################################
 # Extends echo command.
 # Globals:
-#   see echo command.
+#   See echo command.
+#   COLORS
 # Arguments:
-#   see echo command.
+#   See echo command.
 #   -error: display a message with red color.
 #   -status <error_code> <error_message>: display a message either with green color if success or red if fail.
+# Outputs:
+#   Write message to STDOUT
 # Returns:
+#   None.
+# Exits:
 #   None.
 #######################################
 echo() {
-	local -r option="${1}" code="${2}" error="${3}"
+	local -r option="${1}"
+	local -r error_code="${2}"
+	local -r error_message="${3}"
 
 	case "${option}" in
 		"-error")
-			echo -e "\e[31m"${code}"\e[0m"
+			echo -e "\e[31m${error_code}\e[0m"
 			;;
 		"-status")
-			if [[ "${code}" -eq 0 ]]; then
-				echo -e " "${COLORS[GREEN]}"[success]"${COLORS[RESET]}""
+			if [[ "${error_code}" -eq 0 ]]; then
+				echo -e " ${COLORS[GREEN]}[success]${COLORS[RESET]}"
 			else
-				echo -e " "${COLORS[RED]}"[fail]: "${error}${COLORS[RESET]}""
+				echo -e " ${COLORS[RED]}[fail]: ${error_message}${COLORS[RESET]}"
 			fi
 			;;
 		*)
@@ -45,26 +52,32 @@ echo() {
 }
 
 #######################################
-# Takes two strings and checks whether they are the same based on the character strings.
-# Inspired from https://github.com/torokmark/assert.sh
+# Takes two strings and checks whether they are the same based on the character strings. Inspired from https://github.com/torokmark/assert.sh
 # Globals:
 #   None.
 # Arguments:
-#   -expected: expected string.
-#   -actual: current string that should be the same as expected.
-#   -message (optional): message to display if strings are not equals.
+#   <expected>: expected string.
+#   <actual>: current string that should be the same as expected.
+#   <message>: message to display if strings are not equals.
+# Outputs:
+#   Write <message> to STDERR
 # Returns:
-#   -0 if strings are equals or exit with code error 99.
+#   0: if strings are equals.
+# Exits:
+#   99: if arguments are missing.
+#   98: if strings are not equals.
 #######################################
 assert_eq() {
-	local -r ASSERT_FAILED=99
+	local -r -i PARAM_FAILED=99
+	if (( $# < 3 )); then
+		echo -e "usage: assert_eq <expected> <actual> <message>" 1>&2
+		exit ${PARAM_FAILED}
+	fi
+	
+	local -r -i ASSERT_FAILED=98
 	local -r expected="${1}"
 	local -r actual="${2}"
-	local msg
-
-	if [[ "$#" -ge 3 ]]; then
-		msg="${3}"
-	fi
+	local -r message="${3}"
 
 	if [[ "${expected}" == "${actual}" ]]; then
 		return 0
@@ -72,8 +85,88 @@ assert_eq() {
 		# An error occured, retrieved the line and the name of the script where
 		# it happend
 		set $(caller)
-		echo -e "Assertion failed: \""${expected}" == "${actual}"\" :: "${msg}"" 1>&2
+		echo -e "Assertion failed: \""${expected}" == "${actual}"\" :: "${message}"" 1>&2
 		echo -e "File \"${0}\", line ${1}" 1>&2
-		exit "${ASSERT_FAILED}"
+		exit ${ASSERT_FAILED}
+	fi
+}
+
+#######################################
+# Takes two integer and checks whether the left is less than the right one. Inspired from https://github.com/torokmark/assert.sh
+# Globals:
+#   None.
+# Arguments:
+#   <left>: left integer that should be less than the right one
+#   <right>: right integer.
+#   <message>: message to display if the left integer is not less than the right one.
+# Outputs:
+#   Write <message> to STDERR
+# Returns:
+#   0: if the left is less than right.
+# Exits:
+#   99: if arguments are missing.
+#   98: if left is not less than right.
+#######################################
+assert_lt() {
+	local -r -i PARAM_FAILED=99
+	if (( $# < 3 )); then
+		echo -e "usage: assert_lt <left> <right> <message>" 1>&2
+		exit ${PARAM_FAILED}
+	fi
+	
+	local -r -i ASSERT_FAILED=98
+	local -r -i left=${1}
+	local -r -i right=${2}
+	local -r message="${3}"
+
+	if (( ${left} < ${right} )); then
+		return 0
+	else
+		# An error occured, retrieved the line and the name of the script where
+		# it happend
+		set $(caller)
+		echo -e "Assertion failed: \""${left}" < "${right}"\" :: "${message}"" 1>&2
+		echo -e "File \"${0}\", line ${1}" 1>&2
+		exit ${ASSERT_FAILED}
+	fi
+}
+
+#######################################
+# Takes two integer and checks whether the left is greater than or equal to the right one. Inspired from https://github.com/torokmark/assert.sh
+# Globals:
+#   None.
+# Arguments:
+#   <left>: left integer that should be greater than or equal to the right one.
+#   <right>: right integer.
+#   <message>: message to display if the left integer is not greater than or equal to the right one.
+# Outputs:
+#   Write <message> to STDERR
+# Returns:
+#   0: if the left is greater than or equal to the right.
+# Exits:
+#   99: if arguments are missing.
+#   98: if left is not greater than or equal to the right.
+#######################################
+assert_ge() {
+	local -r -i PARAM_FAILED=99
+	if (( $# < 3 )); then
+		echo -e "usage: assert_ge <left> <right> <message>" 1>&2
+		exit ${PARAM_FAILED}
+	fi
+	
+	local -r -i ASSERT_FAILED=98
+	local -r -i left=${1}
+	local -r -i right=${2}
+	local -r message="${3}"
+
+	if (( ${left} >= ${right} )); then
+		return 0
+	else
+		# An error occured, retrieved the line and the name of the script where
+		# it happend
+		set $(caller)
+		echo -e "Assertion failed: \""${left}" >= "${right}"\" :: "${message}"" 1>&2
+		echo -e "File \"${0}\", line ${1}" 1>&2
+		exit ${ASSERT_FAILED}
 	fi
 }
