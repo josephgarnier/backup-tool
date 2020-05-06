ca0b6a24768235b9751c4d36f1eb6ab416413eba
Joseph Garnier <garnjose@gmail.com>
2020-05-06 13:41:38 +0200

    add log_error function

--- src/log.sh
+++ src/log.sh
@@ -13,9 +13,16 @@ source "${DIR}/global.sh"
 #######################################
 # PRIVATE
 #######################################
+_log_header() {
+	local -n ref_header=${1}
+	ref_header="$(date +"%Y/%m/%d %H:%M:%S") [$$]"
+}
+
 _log() {
 	local -r text="${1}"
-	echo -e "$(date +"%Y/%m/%d %H:%M:%S") [$$] ${text}" |& tee -a "${PROJECT_LOG_FILE}"
+	local header=""
+	_log_header header
+	echo -e "${header} ${text}" |& tee -a "${PROJECT_LOG_FILE}"
 }
 
 #######################################
@@ -36,6 +43,29 @@ _log() {
 #   99: if arguments are missing.
 #######################################
 log_info() {
+	local -r -i PARAM_FAILED=99
+	if (( $# < 1 )); then
+		echo -e "usage: log_info <message>" 1>&2
+		exit ${PARAM_FAILED}
+	fi
+
+	_log "${1}"
+}
+
+#######################################
+# Log text with info error. Inspired from https://github.com/fredpalmer/log4bash/blob/master/log4bash.sh.
+# Globals:
+#   PROJECT_LOG_FILE.
+# Arguments:
+#   <message>: text to log.
+# Outputs:
+#   None.
+# Returns:
+#   None.
+# Exits:
+#   99: if arguments are missing.
+#######################################
+log_error() {
 	local -r -i PARAM_FAILED=99
 	if (( $# < 1 )); then
 		echo -e "usage: log_info <message>" 1>&2
