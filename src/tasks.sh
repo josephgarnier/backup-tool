99cc276a129fb3351d6ec3635cd4b32a6b9da86d
Joseph Garnier <garnjose@gmail.com>
2021-04-11 16:52:53 +0200

    Move the "Digital Garden" project

--- src/tasks.sh
+++ src/tasks.sh
@@ -652,7 +652,7 @@ incremental_save_to_dropbox() {
 	
 	# Save with rsync command
 	local -r -a SRC_DIRS=( \
-		"/home/joseph/Documents/Travail/Ingenerie/Projets/Digital_Garden" \
+		"/home/joseph/Documents/1_Projects/Digital_Garden" \
 		"/home/joseph/Documents/Travail/Recherche/Projets/BeInG" \
 	)
 	local -r DROPBOX_DEST_DIR="/home/joseph/Dropbox/Backup"
