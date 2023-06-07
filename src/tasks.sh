de51568a227e6167f66f15214a6b399437b10f64
Joseph Garnier <garnjose@gmail.com>
2023-06-07 11:05:20 +0200

    rewrite zotero according to new backup policy

--- src/tasks.sh
+++ src/tasks.sh
@@ -640,15 +640,21 @@ zettlr() {
 zotero() {
 	echo -e "Backup Zotero."
 
-	local -r DEST_DIR="/home/joseph/Documents/Travail/Logiciels_Outils_et_Configurations/Latex/Zotero"
-	local -r OUTPUT_ZIP_NAME="Zotero.zip"
+	local -r DEST_DIR="/home/joseph/6_Sauvegardes"
+	local -r OUTPUT_BACKUP_DIR_NAME="zotero_linux_$(date +%F)"
+	local -r OUTPUT_BACKUP_DIR="${PROJECT_TEMP_DIR}/${OUTPUT_BACKUP_DIR_NAME}"
+	local -r OUTPUT_ZIP_NAME="${OUTPUT_BACKUP_DIR_NAME}.zip"
 	local -r OUTPUT_ZIP_FILE="${PROJECT_TEMP_DIR}/${OUTPUT_ZIP_NAME}"
 
-	echo -ne "  copy data files to temp directory..."
-	local error=$(cp --parents --archive "/home/joseph/.zotero" "${PROJECT_TEMP_DIR}" 2>&1 1>/dev/null) #-a is same as -dR --preserve=all. It preserve mode, ownership and timestamps.
+	echo -ne "  create the temp backup directory \"${OUTPUT_BACKUP_DIR_NAME}\"."
+	local error=$(mkdir "${OUTPUT_BACKUP_DIR}" 2>&1 1>/dev/null)
 	echo -status "${?}" "${error}"
 
-	echo -ne "  create a zip file with content of temp directory and clean it..."
+	echo -ne "  copy data folder to temp directory..."
+	error=$(cp --archive "/home/joseph/.zotero" "${OUTPUT_BACKUP_DIR}" 2>&1 1>/dev/null) #-a is same as -dR --preserve=all. It preserve mode, ownership and timestamps.
+	echo -status "${?}" "${error}"
+
+	echo -ne "  create the final zip file and clean temp directory..."
 	error=$((cd "${PROJECT_TEMP_DIR}" && zip --recurse-paths --move "${OUTPUT_ZIP_NAME}" *) 2>&1 1>/dev/null)
 	echo -status "${?}" "${error}"
 
